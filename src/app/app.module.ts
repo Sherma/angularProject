11d84bbbb4a117bd92265b6c1e56edcc02cf5c41
Sasha Nyzhnyk <lesteros555@gmail.com>
2022-05-06 12:05:59 +0300

    last lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
+import { KontaktyComponent } from './components/kontakty/kontakty.component';
+
+const appRoutes: Routes = [
+  {path: '', component:GameComponent},
+  {path: 'about', component:KontaktyComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    KontaktyComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
