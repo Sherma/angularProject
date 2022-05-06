790e9137802e3b379abc2fffcdc755d161503af2
Sasha Nyzhnyk <lesteros555@gmail.com>
2022-05-06 10:57:47 +0300

    add inteface on site

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -7,9 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+    name:string = 'Counter-Strike';
+    price:number = 600;
+    version:string = '2.35.1';
+    game_type!: Game_type;
+    options:string[] | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+
+this.game_type = {
+  ftype : 'Shouter',
+  stype : '3d-Action',
+  ttype : 'Simulator'
+};
+this.options = ["Top 1 online","Tournaments","Cybersport"];
   }
 
+  gameSelect(gameName:string){
+    if(gameName=='cs')
+    {
+    this.name = 'Counter-Strike';
+    this.price = 600;
+    this.version = '2.35.1';
+    this.game_type = {
+      ftype : 'Shouter',
+      stype : '3d-Action',
+      ttype : 'Simulator'
+    };
+    this.options = ["Top 1 online","Tournaments","Cybersport"];
+      }
+      else if (gameName == 'dota')
+      {
+        this.name = 'Dota 2';
+        this.price = 100;
+        this.version = '2.00.1';
+        this.game_type = {
+          ftype : 'RPG',
+          stype : '3d-Action',
+          ttype : 'Simulator'
+        };
+        this.options = ["Top 2 online","Tournaments","Cybersport"];
+      }
+      else
+      {
+        this.name = 'Fortnite';
+        this.price = 500;
+        this.version = '1.1.16';
+        this.game_type = {
+          ftype : 'Strategy',
+          stype : '3d-Action',
+          ttype : 'Simulator'
+        };
+        this.options = ["Top 5 online","Tournaments","Cybersport"];
+      }
+
+
+  }
+
+}
+
+
+
+interface Game_type{
+  ftype: string;
+  stype: string;
+  ttype: string;
 }
